746ed5dc25d4382981a4d682b97c2a6a565fe5ab
mennatallahAhmed <139142641+mennatallah222@users.noreply.github.com>
2024-05-13 06:38:36 +0300

    Update Card.java

--- projjj/src/main/java/Card.java
+++ projjj/src/main/java/Card.java
@@ -1,3 +1,4 @@
+package Entities;
 
 import java.io.Serializable;
 import java.util.*;
@@ -5,6 +6,7 @@ import java.util.*;
 import javax.ejb.Stateless;
 import javax.json.JsonValue;
 import javax.persistence.*;
+
 
 @Stateless
 @Entity
@@ -17,7 +19,7 @@ public class Card implements Serializable{
     public String description;
     @ElementCollection(fetch = FetchType.EAGER)
     public List<String> comments;
-    
+    public String status;
     @OneToOne
     @JoinColumn(name="assigneeId")
     private User assignee;
@@ -25,38 +27,70 @@ public class Card implements Serializable{
     @ManyToOne(fetch=FetchType.EAGER)
     @JoinColumn(name = "listId")
     public Lists list;
+    
+    @ManyToOne(fetch=FetchType.EAGER)
+    @JoinColumn(name="sprintId")
+    private Sprint sprint;
+    
+    private int storyPoint;
+    
+    
+    public List<String> getComments() {
+    return comments;
+  }
 
-	public Long getId() {
-		// TODO Auto-generated method stub
-		return cardId;
-	}
+  public Long getId() {
+    // TODO Auto-generated method stub
+    return cardId;
+  }
 
-	public String getDescription() {
-		// TODO Auto-generated method stub
-		return description;
-	}
+  public String getDescription() {
+    // TODO Auto-generated method stub
+    return description;
+  }
 
-	public void setDescription(String desc) {
-		// TODO Auto-generated method stub
-		this.description=desc;
-	}
+  public void setDescription(String desc) {
+    // TODO Auto-generated method stub
+    this.description=desc;
+  }
 
-	public void setComments(List<String> c) {
-		// TODO Auto-generated method stub
-		this.comments=c;
-	}
+  public void setComments(List<String> c) {
+    // TODO Auto-generated method stub
+    this.comments=c;
+  }
 
-	public String getName() {
-		return name;
-	}
+  public String getName() {
+    return name;
+  }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+  public void setName(String name) {
+    this.name = name;
+  }
 
-	public void setUser(User u) {
-		// TODO Auto-generated method stub
-		this.assignee=u;
-	}
-   
+  public void setUser(User u) {
+    // TODO Auto-generated method stub
+    this.assignee=u;
+  }
+  public void setStatus(String status) {
+      this.status = status;
+  }
+
+  public String getStatus() {
+      return status;
+  }
+
+  public int getStoryPoint() {
+    return storyPoint;
+  }
+
+  public void setStoryPoint(int storyPoint) {
+    this.storyPoint = storyPoint;
+  }
+
+  public void setSprint(Sprint s) {
+    this.sprint=s;
+  }
+  public Sprint gdetSprint() {
+    return sprint;
+  }
 }
